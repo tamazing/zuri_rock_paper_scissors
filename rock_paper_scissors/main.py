5d5b4aaf73470a64cb6ee293e8b7c2994ae90b25
tamazing <taiwolasaki@gmail.com>
2022-06-06 00:35:13 +0100

    commit main.py

---
+++ rock_paper_scissors/main.py
@@ -0,0 +1,37 @@
+import random
+
+while True:
+    player = input(
+        "Pick an option between rock, paper or scissors): ")
+    R = "rock"
+    P = "paper"
+    S = "scissors"
+    options = [R, P, S]
+
+    cpu = random.choice(options)
+    print(f"\nPlayer {player.title()} : CPU {cpu.title()}.\n")
+
+    if player == cpu:
+        print(f"You both chose {player}. It's a tie!")
+        continue
+    elif player == "rock":
+        if cpu == "scissors":
+            print("Rock beats scissors! You win!")
+        else:
+            print("Paper beats rock! You lose.")
+        break
+    elif player == "paper":
+        if cpu == "rock":
+            print("Paper beats rock! You win!")
+        else:
+            print("Scissors beats paper! You lose.")
+        break
+    elif player == "scissors":
+        if cpu == "paper":
+            print("Scissors beats paper! You win!")
+        else:
+            print("Rock beats scissors! You lose.")
+        break
+    else:
+        print("You picked an invalid option")
+        continue
